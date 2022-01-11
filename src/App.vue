47c4a915ae4bd8a654081dc740f91a166e16f814
dampion <evanchen79@gmail.com>
2022-01-11 11:09:58 +0800

    Redirect Add

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>redirecting...</h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    const numbigfive = Math.floor(Math.random() * 10)
+    if (numbigfive>4) {
+      location.href = 'https://www.surveycake.com/s/G29GP' 
+    } else {
+      location.href = 'https://www.surveycake.com/s/DnQGz'
+    }
+  },
+}
+</script>
