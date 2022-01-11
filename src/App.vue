2d3d40d48b7416e6504b06a6890895b8db6d20ed
dampion <evanchen79@gmail.com>
2022-01-11 14:00:59 +0800

    [Change] bug fix

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,21 @@ export default {
   },
   created() {
     const numbigfive = Math.floor(Math.random() * 10)
-    if (numbigfive>4) {
-      location.href = 'https://www.surveycake.com/s/G29GP' 
-    } else {
-      location.href = 'https://www.surveycake.com/s/DnQGz'
-    }
+
+    location.href = [
+      'https://www.surveycake.com/s/DnQGz',
+      'https://www.surveycake.com/s/laXqr',
+      'https://www.surveycake.com/s/naWXm',
+      'https://www.surveycake.com/s/aLo7p',
+      'https://www.surveycake.com/s/B2XyV',
+      'https://www.surveycake.com/s/paWmV',
+      'https://www.surveycake.com/s/16bN1',
+      'https://www.surveycake.com/s/DnQA7',
+      'https://www.surveycake.com/s/gobLq',
+      'https://www.surveycake.com/s/G29XK',
+      'https://www.surveycake.com/s/RN3a8',
+      'https://www.surveycake.com/s/eA4bm',
+    ][numbigfive]
   },
 }
 </script>
