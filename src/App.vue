18041da91a85262cc5a51cbd0c17b3e13605108e
dampion <evanchen79@gmail.com>
2022-03-30 20:23:29 +0800

    [Change] update survey list

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,10 @@ export default {
   },
   created() {
     const list = [
-      'https://www.surveycake.com/s/laa7x',
-      'https://www.surveycake.com/s/8llvY',
-      'https://www.surveycake.com/s/YxxB2',
-      'https://www.surveycake.com/s/0PP6L',
-      'https://www.surveycake.com/s/3MpeR',
-      'https://www.surveycake.com/s/qa3mm',
-      'https://www.surveycake.com/s/kaYQ4',
-      'https://www.surveycake.com/s/Vr9Bn',
-      'https://www.surveycake.com/s/la9LP',
-      'https://www.surveycake.com/s/6MleO',
-      'https://www.surveycake.com/s/aLNr4',
-      'https://www.surveycake.com/s/DnzbX',
+      'https://www.surveycake.com/s/Z8kl2',
+      'https://www.surveycake.com/s/3M36b',
+      'https://www.surveycake.com/s/XWAZr',
+      'https://www.surveycake.com/s/4Mong',
     ]
     const numbigfive = Math.floor(Math.random() * list.length)
 
