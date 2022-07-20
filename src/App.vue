980cc26da5a5af0627e990d223992768f94a6f99
dampion <evanchen79@gmail.com>
2022-07-20 13:48:40 +0800

    [Change] 2022/07/20 professor's survey

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@ export default {
   },
   created() {
     const list = [
-      'https://www.surveycake.com/s/Z8kl2',
-      'https://www.surveycake.com/s/3M36b',
-      'https://www.surveycake.com/s/XWAZr',
-      'https://www.surveycake.com/s/4Mong',
+      'https://forms.gle/ZAceUHoP1MMAFZAN8',
+      'https://forms.gle/jABhLuHTp7JVMYnL8',
+      'https://forms.gle/ZYUnipJNA9Sd6kZ6A',
+      'https://forms.gle/XtSqtC66b7okZjSt6',
+      'https://forms.gle/zBtyiaMrbedPAGvY8',
+      'https://forms.gle/sPu1aF4R4piCx6h89',
+      'https://forms.gle/4H7cjU1UnUPUEpAn7',
+      'https://forms.gle/77ZQeZcW1DozzdNc8',
     ]
     const numbigfive = Math.floor(Math.random() * list.length)
 
